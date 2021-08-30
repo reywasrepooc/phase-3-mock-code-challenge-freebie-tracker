ee5cbcadb9e76c24e5b888c0e8e68a38344486df
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-30 10:59:48 -0400

    commit tree 88a71ffd4e41c4b09ba02d0d7810342a524afb59

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def received_one?(item_name)
+      freebies.any? do |freebie|
+        freebie.item_name == item_name
+    end
+
+    def give_away(dev, freebie)
+      freebie.update(dev: dev) unless freebie.dev != self
+    end
 
 end
